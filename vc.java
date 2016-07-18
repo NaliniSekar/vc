93d11dfbde26850f9a2bc90001e0b182bf396898
NaliniSekar <NaliniSekar@users.noreply.github.com>
2016-07-18 00:14:55 -0700

    Update and rename README.md to vc.java

---
+++ vc.java
@@ -0,0 +1,15 @@
+# vc
+Public static void main(String args[])
+{
+System.out.println("enter input");
+Scanner in = new Scanner();
+char b=in.nextChar();
+if(b==a || b== e ||b==i||b==o||b==u )
+{
+System.out.println("char is vowel ");
+}
+else
+{
+System.out.println("char is consonant");
+}
+}
